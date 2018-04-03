f3fac56c21a1e21b3274e36ec2bdabb41fa3296d
jeremyprice97 <36086500+jeremyprice97@users.noreply.github.com>
2018-04-02 21:53:10 -0400

    Started SQL db

---
+++ app/src/main/java/cmsc355/assignmnet4part2/Pairs.java
@@ -0,0 +1,29 @@
+package cmsc355.assignmnet4part2;
+
+/**
+ * Created by tenni_000 on 4/2/2018.
+ */
+
+public class Pairs {
+    int id;
+    String synonym, antonym;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return this.id;
+    }
+    public void setSynonym(String synonym) {
+        this.synonym = synonym;
+    }
+    public String getSynonym() {
+        return this.synonym;
+    }
+    public void setAntonym(String antonym) {
+        this.antonym = antonym;
+    }
+    public String getAntonym(String antonym) {
+        return this.antonym;
+    }
+}
